05e44f1a576785d0f1f038b1bf385db8bb93b354
Suman Panigrahi <psuman1406@gmail.com>
2025-10-30 06:49:04 +0000

    In the home page only after the first section add the other sections als

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import { OceanHero } from '@/components/ocean-hero';
+import AboutPage from './about/page';
+import DakargaladhyayaPage from './dakargaladhyaya/page';
+import GranthaPage from './grantha/page';
+import PublishedPage from './published/page';
 
 export default function Home() {
   return (
     <div className="relative">
       <OceanHero />
+      <div className="space-y-16 py-16">
+        <DakargaladhyayaPage />
+        <GranthaPage />
+        <PublishedPage />
+        <AboutPage />
+      </div>
     </div>
   );
 }
