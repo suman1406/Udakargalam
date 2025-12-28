8c1bcbf93c1b3d446545ee178e91b80d7efb2bc2
Suman Panigrahi <psuman7155@gmail.com>
2025-12-28 11:29:01 +0530

    feat: add PDF data and placeholder images for multiple languages.

--- src/lib/pdf-data.ts
+++ src/lib/pdf-data.ts
@@ -8,11 +8,11 @@ export type PdfInfo = {
 };
 
 export const pdfs: PdfInfo[] = [
-  { id: 'samskrita', localeKey: 'samskrita', name: 'Samskrita / संस्कृतम्', file: 'https://drive.google.com/file/d/1QNlnLvVc00Sx-VoV77Kc55incDGCYm3B/view?usp=sharing', thumbId: 'pdf-thumb-1' },
-  { id: 'hindi', localeKey: 'hindi', name: 'Hindi— हिन्दि', file: 'https://drive.google.com/file/d/1SnQ64oKuLy0vC1FvCvV8PqE8QMHeQZKr/view?usp=sharing', thumbId: 'pdf-thumb-2' },
-  { id: 'english', localeKey: 'english', name: 'English— आङ्ग्लम्', file: 'https://drive.google.com/file/d/1fi15BJbIarU9f7HG33ClJE0642jihBvo/view?usp=sharing', thumbId: 'pdf-thumb-3' },
-  { id: 'kannada', localeKey: 'kannada', name: 'Kannada— कन्नड', file: 'https://drive.google.com/file/d/1E7MjajFsAoNHFTK8iRlULkpMtWUpBqyP/view?usp=sharing', thumbId: 'pdf-thumb-4' },
-  { id: 'marathi', localeKey: 'marathi', name: 'Marathi— मराठी', file: 'https://drive.google.com/file/d/1JkFeDGtk7rLNX9zsK9WVWoOYpKlIjfr9/view?usp=sharing', thumbId: 'pdf-thumb-5' },
-  { id: 'bengali', localeKey: 'bengali', name: 'Bengali— बेङ्गालि', file: 'https://drive.google.com/file/d/1rRpUHo_GCMw3ggT08sY922NaZyDLlMJb/view?usp=sharing', thumbId: 'pdf-thumb-6' },
-  { id: 'malayalam', localeKey: 'malayalam', name: 'Malayalam— मलयालम्', file: 'https://drive.google.com/file/d/1tVMOxDBZ42XiZknMxbcHwHghUy5ZB6pq/view?usp=sharing', thumbId: 'pdf-thumb-7' },
+  { id: 'samskrita', localeKey: 'samskrita', name: 'Samskrita / संस्कृतम्', file: 'https://drive.google.com/file/d/1KZIrKVLAQzuFlGuqDCaN8i8Z5djJp_4d/view?usp=drive_link', thumbId: 'pdf-thumb-1' },
+  { id: 'hindi', localeKey: 'hindi', name: 'Hindi— हिन्दि', file: 'https://drive.google.com/file/d/14Rbt-YQ3VXEfrUCo-_EP-mZG_HjdAIm4/view?usp=drive_link', thumbId: 'pdf-thumb-2' },
+  { id: 'english', localeKey: 'english', name: 'English— आङ्ग्लम्', file: 'https://drive.google.com/file/d/1-aXiGm6NRbVe1jmEgwWZfOOEUnd1b9iA/view?usp=drive_link', thumbId: 'pdf-thumb-3' },
+  { id: 'kannada', localeKey: 'kannada', name: 'Kannada— कन्नड', file: 'https://drive.google.com/file/d/1xQXw4SmQ30HkMqXGUm1qfRIBjZLqCLk9/view?usp=drive_link', thumbId: 'pdf-thumb-4' },
+  { id: 'marathi', localeKey: 'marathi', name: 'Marathi— मराठी', file: 'https://drive.google.com/file/d/1Lrbwf42HXjG2rPCiYKvSKYtjQzgMsDnh/view?usp=drive_link', thumbId: 'pdf-thumb-5' },
+  { id: 'bengali', localeKey: 'bengali', name: 'Bengali— बेङ्गालि', file: 'https://drive.google.com/file/d/13sW6HyhGBdqcjYajgmsMa71KNjK8E7wT/view?usp=drive_link', thumbId: 'pdf-thumb-6' },
+  { id: 'malayalam', localeKey: 'malayalam', name: 'Malayalam— मलयालम्', file: 'https://drive.google.com/file/d/1pYvicwa9z4ehhW8RT5ZtBImtsRhsGOST/view?usp=drive_link', thumbId: 'pdf-thumb-7' },
 ];
