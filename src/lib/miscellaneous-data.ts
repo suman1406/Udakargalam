f27dfd5b0281bb7fafecc7d9a0d43e893f806590
Suman Panigrahi <psuman7155@gmail.com>
2026-04-24 10:27:15 +0530

    fix: update name for Brhatsamhita Mula Sloka in miscellaneous PDFs

--- src/lib/miscellaneous-data.ts
+++ src/lib/miscellaneous-data.ts
@@ -4,7 +4,7 @@ export const miscellaneousPdfs: PdfInfo[] = [
   {
     id: 'brhatsamhita-mula-sloka',
     localeKey: 'brhatsamhitaMulaSloka',
-    name: 'Brhatsamhita Mula Sloka',
+    name: 'Mūlaślokāḥ – Udakārgala-adhyāyaḥ',
     file: 'https://drive.google.com/file/d/1ecJ80yle1q1Q60VJk_6b6otEDUaI1glx/view?usp=sharing',
     thumbId: 'pdf-thumb-10',
   },
